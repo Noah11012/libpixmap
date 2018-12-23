5cfe9653dd497d98b34f135d45a686dbd249b543
Noah <1101qbtext@gmail.com>
2018-12-23 17:15:57 -0500

    Added function for reading and parsing numbers from image format

--- src/pixmap.c
+++ src/pixmap.c
@@ -2,11 +2,13 @@
 
 struct PixMapImage
 {
-    FILE *image_file;
+    FILE *file;
     u8   *pixels;
     int   width;
     int   height;
 };
+
+static u32 pixmap_image_read_number(FILE *file, u32 *output);
 
 void pixmap_image_new(PixMapImage **image, u32 width, u32 height)
 {
@@ -27,13 +29,13 @@ void pixmap_image_new(PixMapImage **image, u32 width, u32 height)
         return;
     }
 
-    new_image->image_file = 0;
+    new_image->file = 0;
     *image = new_image;
 }
 
 void pixmap_image_free(PixMapImage *image)
 {
-    fclose(image->image_file);
+    fclose(image->file);
     free(image->pixels);
     free(image);
 }
@@ -46,8 +48,8 @@ void pixmap_image_open(PixMapImage **image, char const *file_path)
         return;
     }
 
-    FILE *image_file = 0;
-    if(!(image_file = fopen(file_path, "r")))
+    FILE *file = 0;
+    if(!(file = fopen(file_path, "r")))
     {
         *image = 0;
         return;
@@ -57,7 +59,7 @@ void pixmap_image_open(PixMapImage **image, char const *file_path)
     // Later, we will support P6
     
     u8 signature[2] = { 0 };
-    fread(signature, sizeof(u8), 2, image_file);
+    fread(signature, sizeof(u8), 2, file);
     if(!(signature[0] == 'P' && signature[1] == '3'))
     {
         *image = 0;
@@ -67,71 +69,76 @@ void pixmap_image_open(PixMapImage **image, char const *file_path)
     // PPM images allow for comments virtually ANYWHERE in the file
     // We have to keep this in mind
 
-    int is_comment = 0;
-    int image_width = 0;
-    int image_height = 0;
-    int c = 0;
+    u32 image_width = 0;
+    u32 image_height = 0;
+    u32 number = 0;
 
     // Extract width and height first
     
     // width
     
-    while((c = fgetc(image_file)) && (!feof(image_file)))
+    if(pixmap_image_read_number(file, &image_width) == -1)
     {
-        if(c == '#')
-            is_comment = 1;    
-        else if(c == '\n')
-            is_comment = 0;
-        else if(is_comment)
-            continue;
-
-        if(isdigit(c))
-        {
-            image_width *= 10;
-            image_width += c - '0';
-        }
-
-        if(isspace(c))
-            break;
+        fclose(file);
+        *image = 0;
+        return;
     }
 
     // height
     
-    while((c = fgetc(image_file)) && (!feof(image_file)))
+    if(pixmap_image_read_number(file, &image_width) == -1)
     {
-        if(c == '#')
-            is_comment = 1;    
-        else if(c == '\n')
-            is_comment = 0;
-        else if(is_comment)
-            continue;
-
-        if(isdigit(c))
-        {
-            image_height *= 10;
-            image_height += c - '0';
-        }
-
-        if(isspace(c))
-            break;
+        fclose(file);
+        *image = 0;
+        return;
     }
 
     PixMapImage *new_image = 0;
-    pixmap_image_new(&new_image, image_width, image_height);
+    pixmap_image_new(&new_image, image_width, number);
     if(!new_image)
     {
         *image = 0;
         return;
     }
 
-    if(!(new_image->pixels = malloc(sizeof(u8) * (image_width * image_height * 3))))
+    if(!(new_image->pixels = malloc(sizeof(u8) * (image_width * number * 3))))
     {
         *image = 0;
         return;
     }
 
     new_image->width = image_width;
-    new_image->height = image_height;
+    new_image->height = number;
 
     // TODO(noah): Parse pixels
+}
+
+static u32 pixmap_image_read_number(FILE *file, u32 *output)
+{
+    int is_comment;
+    int c = 0;
+    int number = 0;
+    while((c = fgetc(file)) && (!feof(file)))
+    {
+        if(c == '#')
+            is_comment = 1;    
+        else if(c == '\n')
+            is_comment = 0;
+        else if(is_comment)
+            continue;
+
+        if(isdigit(c))
+        {
+            number *= 10;
+            number += c - '0';
+        }
+
+        if(isspace(c))
+        {
+            *output = number;
+            return 0;
+        }
+    }
+
+    return -1;
 }
